d952438f54cfba41241479a2b53e42c57307d65b
Alexander <william.schwarer@gmial.com>
2025-05-01 11:15:38 -0600

    Interactive test object complete Some visuals added when the objects is picked up

--- src/Interface.js
+++ src/Interface.js
@@ -1,34 +1,40 @@
 const root = document.documentElement;
         
 const gridBlocks = document.getElementById("gridPage");
+let holding = false;
 
 // 2nd test object
 const ob = document.createElement("div");
 ob.className = "gridSpanTest";
 
 let row, column;
+let pos;
+const gridBlockLength = 2.34;
 
 //  Adding grid background
 for (let i = 0; i < 300; i++){
     const gridSpace = document.createElement("div");
     gridSpace.className = "gridBlock";
     row = Math.floor(i/20)+1;
-    column = i%20;
+    column = (i%20)+1;
+    pos = row.toString().concat(",", column.toString());
     gridSpace.style.gridRowStart = row;
     gridSpace.style.gridColumnStart = column;
+    gridSpace.id = pos;
+    gridSpace.addEventListener("click", place.bind(this, row, column));
     gridBlocks.appendChild(gridSpace);
 }
 
 // creating test object
 const divSpan = document.createElement("div");
 const img = document.createElement("img");
-img.src = "images/swordHorizontal.jpg";
+img.src = "images/swordVertical.jpg";
 img.className = "objectIcon";
 divSpan.className = "gridSpanTest";
 divSpan.appendChild(img);
+divSpan.style.width = ((gridBlockLength*2)-0.5).toString().concat("vw");
+divSpan.style.height = ((gridBlockLength*5)-0.5).toString().concat("vw");
 gridBlocks.appendChild(divSpan);
-
-let holding = false;
 
 // get mouse position inside the grid
 gridBlocks.addEventListener("mousemove", e => {
@@ -36,14 +42,32 @@ gridBlocks.addEventListener("mousemove", e => {
     divSpan.style.setProperty('--mousey', e.clientY + "px");
 });
 
-divSpan.addEventListener("click", pickup);
+divSpan.addEventListener("click", handle);
 
-function pickup() {
-    if (holding == false) {
+function handle() {
+    if (!holding) {
+        console.log("pickup");
         divSpan.style.position = "absolute";
-        divSpan.style.backgroundColor = "blue";
-        // divSpan.style.left = "var(--mousex)";
-        // divSpan.style.top = "var(--mousey)";
+        divSpan.style.pointerEvents = "none";
+        divSpan.style.borderWidth = "0.5vw";
+        divSpan.style.opacity = "0.6";
+        holding = true;
     }
-    holding = true;
+}
+
+function place(r, c) {
+    if (holding) {
+        console.log(r.toString().concat(",", c.toString()));
+        row = r;
+        column = c;
+        console.log("place");
+        divSpan.style.position = "static";
+        divSpan.style.gridRowStart = parseInt(r);
+        divSpan.style.gridColumnStart = parseInt(c);
+        divSpan.style.backgroundColor = "white";
+        divSpan.style.pointerEvents = "auto";
+        divSpan.style.borderWidth = "0.25vw";
+        divSpan.style.opacity = "1";
+        holding = false;
+    }
 }
