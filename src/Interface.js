684fe870e0b4bf5d27d650b3d36a92e2e0cde388
Alexander <william.schwarer@gmial.com>
2025-05-02 10:44:32 -0600

    review changes: --b changed to --blockSize prettier formatting applied handle function is now pickupItem place function is now placeItem redundant early testing code removed, including "ob"

--- src/Interface.js
+++ src/Interface.js
@@ -1,28 +1,45 @@
+class item {
+  constructor(width, height, image, name) {
+    this.width = width;
+    this.height = height;
+    this.image = image;
+    this.name = name;
+  }
+  get area() {
+    return this.height * this.width;
+  }
+}
+
 const root = document.documentElement;
-        
+
 const gridBlocks = document.getElementById("gridPage");
+
 let holding = false;
-
-// 2nd test object
-const ob = document.createElement("div");
-ob.className = "gridSpanTest";
-
 let row, column;
 let pos;
 const gridBlockLength = 2.34;
 
+let items = [];
+let sword = new item(2, 5, "images/swordVertical.jpg", "sword");
+items.push(sword);
+
+const slector = document.getElementById("itemSelector");
+// items.forEach(displayItem);
+
+// function displayItem(item) {}
+
 //  Adding grid background
-for (let i = 0; i < 300; i++){
-    const gridSpace = document.createElement("div");
-    gridSpace.className = "gridBlock";
-    row = Math.floor(i/20)+1;
-    column = (i%20)+1;
-    pos = row.toString().concat(",", column.toString());
-    gridSpace.style.gridRowStart = row;
-    gridSpace.style.gridColumnStart = column;
-    gridSpace.id = pos;
-    gridSpace.addEventListener("click", place.bind(this, row, column));
-    gridBlocks.appendChild(gridSpace);
+for (let i = 0; i < 300; i++) {
+  const gridSpace = document.createElement("div");
+  gridSpace.className = "gridBlock";
+  row = Math.floor(i / 20) + 1;
+  column = (i % 20) + 1;
+  pos = row.toString().concat(",", column.toString());
+  gridSpace.style.gridRowStart = row;
+  gridSpace.style.gridColumnStart = column;
+  gridSpace.id = pos;
+  gridSpace.addEventListener("click", place.bind(this, row, column));
+  gridBlocks.appendChild(gridSpace);
 }
 
 // creating test object
@@ -32,42 +49,42 @@ img.src = "images/swordVertical.jpg";
 img.className = "objectIcon";
 divSpan.className = "gridSpanTest";
 divSpan.appendChild(img);
-divSpan.style.width = ((gridBlockLength*2)-0.5).toString().concat("vw");
-divSpan.style.height = ((gridBlockLength*5)-0.5).toString().concat("vw");
+divSpan.style.width = (gridBlockLength * 2 - 0.5).toString().concat("vw");
+divSpan.style.height = (gridBlockLength * 5 - 0.5).toString().concat("vw");
 gridBlocks.appendChild(divSpan);
 
 // get mouse position inside the grid
-gridBlocks.addEventListener("mousemove", e => {
-    divSpan.style.setProperty('--mousex', e.clientX + "px");
-    divSpan.style.setProperty('--mousey', e.clientY + "px");
+gridBlocks.addEventListener("mousemove", (e) => {
+  divSpan.style.setProperty("--mousex", e.clientX + "px");
+  divSpan.style.setProperty("--mousey", e.clientY + "px");
 });
 
 divSpan.addEventListener("click", handle);
 
 function handle() {
-    if (!holding) {
-        console.log("pickup");
-        divSpan.style.position = "absolute";
-        divSpan.style.pointerEvents = "none";
-        divSpan.style.borderWidth = "0.5vw";
-        divSpan.style.opacity = "0.6";
-        holding = true;
-    }
+  if (!holding) {
+    console.log("pickup");
+    divSpan.style.position = "absolute";
+    divSpan.style.pointerEvents = "none";
+    divSpan.style.borderWidth = "0.5vw";
+    divSpan.style.opacity = "0.6";
+    holding = true;
+  }
 }
 
 function place(r, c) {
-    if (holding) {
-        console.log(r.toString().concat(",", c.toString()));
-        row = r;
-        column = c;
-        console.log("place");
-        divSpan.style.position = "static";
-        divSpan.style.gridRowStart = parseInt(r);
-        divSpan.style.gridColumnStart = parseInt(c);
-        divSpan.style.backgroundColor = "white";
-        divSpan.style.pointerEvents = "auto";
-        divSpan.style.borderWidth = "0.25vw";
-        divSpan.style.opacity = "1";
-        holding = false;
-    }
+  if (holding) {
+    console.log(r.toString().concat(",", c.toString()));
+    row = r;
+    column = c;
+    console.log("place");
+    divSpan.style.position = "static";
+    divSpan.style.gridRowStart = parseInt(r);
+    divSpan.style.gridColumnStart = parseInt(c);
+    divSpan.style.backgroundColor = "white";
+    divSpan.style.pointerEvents = "auto";
+    divSpan.style.borderWidth = "0.25vw";
+    divSpan.style.opacity = "1";
+    holding = false;
+  }
 }
